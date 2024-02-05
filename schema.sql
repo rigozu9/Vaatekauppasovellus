0d1d5852e44b78037203cf22a0683164ae0d78fd
Riku <134082872+rigozu9@users.noreply.github.com>
2024-02-05 11:46:03 +0200

    Now can clone the repository from github and use the correctly after following README instructions

---
+++ schema.sql
@@ -0,0 +1,60 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE brands (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE sizes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE clothes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    size VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL,
+    username VARCHAR(255) REFERENCES users(id),
+    image_path VARCHAR(255)
+);
+
+CREATE TABLE clothes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    size VARCHAR(255) NOT NULL,
+    price FLOAT NOT NULL,
+    username VARCHAR(50) REFERENCES users(username),
+    image_path VARCHAR(255)
+);
+
+INSERT INTO categories (name) VALUES ('Tops');
+INSERT INTO categories (name) VALUES ('Bottoms');
+INSERT INTO categories (name) VALUES ('Footwear');
+INSERT INTO categories (name) VALUES ('Other');
+
+
+INSERT INTO brands (name) VALUES ('BAPE');
+INSERT INTO brands (name) VALUES ('NUMBER (N)INE');
+INSERT INTO brands (name) VALUES ('junya watanabe');
+INSERT INTO brands (name) VALUES ('All brands');
+
+INSERT INTO sizes (name) VALUES ('S');
+INSERT INTO sizes (name) VALUES ('M');
+INSERT INTO sizes (name) VALUES ('L');
+INSERT INTO sizes (name) VALUES ('XL');
