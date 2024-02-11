fc01b76671a698fca137e1e84ba839b81fe36ddc
Riku <134082872+rigozu9@users.noreply.github.com>
2024-02-12 00:24:37 +0200

    fly

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 
 app.secret_key = getenv("SECRET_KEY")
 
