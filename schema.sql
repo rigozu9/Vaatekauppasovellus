080982696a7a29dea6b2ba2f905916a7ab7d3192
Riku <134082872+rigozu9@users.noreply.github.com>
2024-02-12 11:39:55 +0200

    made password longer so works with scrypt

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(128) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE categories (
